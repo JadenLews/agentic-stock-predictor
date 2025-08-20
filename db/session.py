aefcb50083cc8e3014707b7f5713b7da5c34b9e5
Jaden Lewis <jlewis@clarku.edu>
2025-08-19 20:54:19 -0400

    Update session.py

--- db/session.py
+++ db/session.py
@@ -5,4 +5,5 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///data/market.db"
 engine = create_engine(DATABASE_URL, future=True, echo=False)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
